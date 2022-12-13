1033a74fecab75a7970c3f1c071875fbb288b9d1
Tode Temov <75773817+temov@users.noreply.github.com>
2022-12-13 16:22:23 +0100

    Add files via upload

---
+++ Homework_2/script.js
@@ -0,0 +1,11 @@
+let humanAgeInput,dogAgeInput,hage,dage;
+
+
+function humanToDog(){
+   dogAgeInput = prompt('Input age of Dog');
+   hage = dogAgeInput/7;
+   console.log(`Dog age is: ${dogAgeInput} , Human age is:${hage}`);
+ 
+};
+
+humanToDog();
