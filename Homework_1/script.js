23144c8db081f52c444f1f57ffbcdcb6bb28f694
Tode Temov <75773817+temov@users.noreply.github.com>
2022-12-13 16:20:54 +0100

    Add files via upload

---
+++ Homework_1/script.js
@@ -0,0 +1,13 @@
+let param;
+let x;
+
+function callFiveTimes(param){
+   
+   console.log(param);
+};
+
+callFiveTimes("Tode Temov");
+callFiveTimes({});
+callFiveTimes(55);
+callFiveTimes(true);
+callFiveTimes(x);
